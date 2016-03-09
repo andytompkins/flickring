470d0b90080a3cdde24bebbcb9ded59af2af2c7e
andytompkins <andy.tompkins@gmail.com>
2016-03-09 10:18:39 -0800

    Added flickrapi, created test call to retrieve 1 random image matching some text about a user.

--- src/scripts/app/controllers/MyCtrl.js
+++ src/scripts/app/controllers/MyCtrl.js
@@ -13,4 +13,26 @@ mCtrls.controller('MyCtrl', function ($scope) {
     console.log("appData:");
     console.dir($scope.appData);
     
+    let flickr = new Flickr({
+      api_key: "84591107291ef03adcc9712424c8e135"
+    });
+    
+    $scope.loadRecord = function() {
+      flickr.photos.search({
+        text: $scope.appData[$scope.current].PrimaryAddress.CITY + "+" + $scope.appData[$scope.current].PrimaryAddress.STATE
+      }, function(err, result) {
+        if (err) { throw new Error(err); }
+        console.dir(result);
+        $scope.images.length = 0;
+        let photo = result.photos.photo[0];
+        let url = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_' + 'm.jpg';
+        $scope.images.push({
+          'url': url,
+          'alt': photo.title
+        });
+      });
+    };
+    
+    $scope.loadRecord();
+    
 });
